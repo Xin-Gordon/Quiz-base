03ead3e7ad4a8092bf04dccd6ef18e33dbf08fb6
gordon <lochengxin@gmail.com>
2021-03-04 18:05:17 +1300

    add turn and false

--- app/src/test/java/TrueFalse.java
+++ app/src/test/java/TrueFalse.java
@@ -1,2 +1,12 @@
 public class TrueFalse {
+
+    private int questionID;
+
+    private boolean answer;
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse){
+        questionID = questionResourceID;
+        answer = trueOrFalse;
+
+    }
 }
